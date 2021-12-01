867c91bee6f0d1391774c1433096be2827733ed2
Othior <splingaire.marvyn@gmail.com>
2021-12-01 18:53:42 +0100

    correction rel

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ function App() {
         class="lien" 
         href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=0%3A1&node-id=236%3A18&viewport=373%2C48%2C0.57&scaling=min-zoom&starting-point-node-id=236%3A18" 
         target="_blank"
+        rel="noreferrer"
       >Vue Admin PC</a>
       <a 
         class="lien" 
         href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=122%3A11&node-id=493%3A435&viewport=373%2C48%2C0.26&scaling=scale-down&starting-point-node-id=493%3A435" 
         target="_blank"
+        rel="noreferrer"
       >Vue Admin mobile</a>
       <a 
         class="lien" 
         href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=197%3A74&node-id=197%3A14&viewport=373%2C48%2C0.23&scaling=scale-down&starting-point-node-id=197%3A14" 
         target="_blank"
+        rel="noreferrer"
       >Vue Planteurs/Agriculteurs</a>
     </div>
   );
