f691502d57a1989ad6915427c76b2e99e8b2d3cb
Othior <splingaire.marvyn@gmail.com>
2021-12-01 18:50:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Groupe Agriculteur</h1>
+      <a 
+        class="lien" 
+        href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=0%3A1&node-id=236%3A18&viewport=373%2C48%2C0.57&scaling=min-zoom&starting-point-node-id=236%3A18" 
+        target="_blank"
+      >Vue Admin PC</a>
+      <a 
+        class="lien" 
+        href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=122%3A11&node-id=493%3A435&viewport=373%2C48%2C0.26&scaling=scale-down&starting-point-node-id=493%3A435" 
+        target="_blank"
+      >Vue Admin mobile</a>
+      <a 
+        class="lien" 
+        href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=197%3A74&node-id=197%3A14&viewport=373%2C48%2C0.23&scaling=scale-down&starting-point-node-id=197%3A14" 
+        target="_blank"
+      >Vue Planteurs/Agriculteurs</a>
     </div>
   );
 }
