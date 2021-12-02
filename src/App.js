bc83e92d7259e11f2cd33ce2412a91ad3624c51d
Othior <splingaire.marvyn@gmail.com>
2021-12-02 09:32:59 +0100

    change de content

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App() {
         href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=0%3A1&node-id=236%3A18&viewport=373%2C48%2C0.57&scaling=min-zoom&starting-point-node-id=236%3A18"
         target="_blank"
         rel="noreferrer"
-      >Vue Admin PC</a>
+      >Vue Admin PlantC PC</a>
       <a
         class="lien"
         href="https://www.figma.com/proto/hlwIYMzkgl7Am9mqLK9hwJ/belgagrow?page-id=122%3A11&node-id=493%3A435&viewport=373%2C48%2C0.26&scaling=scale-down&starting-point-node-id=493%3A435"
         target="_blank"
         rel="noreferrer"
-      >Vue Admin mobile</a>
+      >Vue Admin PlantC mobile</a>
 
     </div>
   );
